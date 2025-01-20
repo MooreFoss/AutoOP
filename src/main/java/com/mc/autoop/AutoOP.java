f07822f15d5ad5614aaa0a7347d7b5809ee47763
MooreFoss <thelmh@outlook.com>
2025-01-20 18:32:21 +0800

    init

---
+++ src/main/java/com/mc/autoop/AutoOP.java
@@ -0,0 +1,17 @@
+package com.mc.autoop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoOP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
